31bfae0605f744450e830e6fa01a28c7024aa720
EchoEclipse24 <arunavb2024@gmail.com>
2026-09-09 13:34:42 +0530

    Update embedding.py

--- Checking_Similarity/embedding.py
+++ Checking_Similarity/embedding.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-newvar=123
+newvar=1
 embedding_model = HuggingFaceEndpointEmbeddings(
     repo_id = "sentence-transformers/all-MiniLM-L6-v2",
     task= "feature-extraction"
