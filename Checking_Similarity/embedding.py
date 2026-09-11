84fdf4229805dbb25b01e8102b851bea088ff8ab
ARUNAV BORTHAKUR <122561140+Arunav47@users.noreply.github.com>
2026-09-11 19:54:53 +0530

    Update embedding.py

--- Checking_Similarity/embedding.py
+++ Checking_Similarity/embedding.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-newvar=12345
+newvar=123456
 embedding_model = HuggingFaceEndpointEmbeddings(
     repo_id = "sentence-transformers/all-MiniLM-L6-v2",
     task= "feature-extraction"
