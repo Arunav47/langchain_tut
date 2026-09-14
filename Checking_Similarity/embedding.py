21e5467ad146d9516119994047f0420bb493e700
ARUNAV BORTHAKUR <122561140+Arunav47@users.noreply.github.com>
2026-09-14 17:37:43 +0530

    Update newvar value in embedding.py

--- Checking_Similarity/embedding.py
+++ Checking_Similarity/embedding.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-newvar=123456786543
+newvar=1234567865432
 embedding_model = HuggingFaceEndpointEmbeddings(
     repo_id = "sentence-transformers/all-MiniLM-L6-v2",
     task= "feature-extraction"
